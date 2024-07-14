3214a9e5ab01018d3202e96f2e9ebcf7975aed27
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-07-14 14:44:16 +0800

    Add the squre pixels for the canvas
    The aspect ratio of the canvas is consistently 1:1.

---
+++ index.js
@@ -0,0 +1,15 @@
+const PXL_LINE_COUNT = 100;
+const TOTAL_PXL_COUNT = PXL_LINE_COUNT ** 2;
+
+const containerDiv = document.querySelector(".container");
+const containerWidth = containerDiv.offsetWidth;
+
+const pxlWidth = containerWidth / PXL_LINE_COUNT;
+const pxlWidthPercent = pxlWidth / containerWidth * 100;
+
+for (let i = 0; i < TOTAL_PXL_COUNT; i++) {
+    const pxlDiv = document.createElement("div");
+    containerDiv.appendChild(pxlDiv);
+    pxlDiv.style.flexBasis = pxlWidthPercent + "%";
+    pxlDiv.classList.add("pixel");
+}
