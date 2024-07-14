4573b5103c4f3214790beada0ba94611a7ac0d33
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-07-14 16:46:56 +0800

    Add button that sets up the canvas on click

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const PXL_LINE_COUNT = 100;
-const TOTAL_PXL_COUNT = PXL_LINE_COUNT ** 2;
-
-const containerDiv = document.querySelector(".container");
-const containerWidth = containerDiv.offsetWidth;
-
-const pxlWidth = containerWidth / PXL_LINE_COUNT;
-const pxlWidthPercent = pxlWidth / containerWidth * 100;
-
-for (let i = 0; i < TOTAL_PXL_COUNT; i++) {
-    const pxlDiv = document.createElement("div");
-    containerDiv.appendChild(pxlDiv);
-    pxlDiv.style.flexBasis = pxlWidthPercent + "%";
-    pxlDiv.classList.add("pixel");
-
-    pxlDiv.addEventListener("mouseenter", colorizePixel)
-}
-
 function colorizePixel() {
     const pixelEl = this;
     pixelEl.style.backgroundColor = "yellow";
 }
+
+function setupCanvas() {
+    const PXL_LINE_COUNT = 100;
+    const TOTAL_PXL_COUNT = PXL_LINE_COUNT ** 2;
+
+    const containerDiv = document.querySelector(".container");
+    const containerWidth = containerDiv.offsetWidth;
+
+    const pxlWidth = containerWidth / PXL_LINE_COUNT;
+    const pxlWidthPercent = pxlWidth / containerWidth * 100;
+
+    for (let i = 0; i < TOTAL_PXL_COUNT; i++) {
+        const pxlDiv = document.createElement("div");
+        containerDiv.appendChild(pxlDiv);
+        pxlDiv.style.flexBasis = pxlWidthPercent + "%";
+        pxlDiv.classList.add("pixel");
+
+        pxlDiv.addEventListener("mouseenter", colorizePixel)
+    }
+}
+
+const setupCanvasButton = document.querySelector("#setup-canvas");
+setupCanvasButton.addEventListener("click", setupCanvas);
